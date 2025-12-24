ae1155d394e1e296272da3e44d8e227384b5ba2e
Phạm Thanh Huy <huyp7922@gmail.com>
2025-12-24 14:52:22 +0700

    haizz

--- utils/jsonDB.js
+++ utils/jsonDB.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const DB_DIR = path.join(__dirname, "../../db");
+const DB_DIR = path.join(__dirname, "../db");
 const loadDB = async (resourceName) => {
   try {
     const data = await fs.readFile(`${DB_DIR}/${resourceName}.json`, "utf8");
